6b26c2f71ca70e27e6f3c330af54e30a0c016866
Ba1oretto <sunjiamu@outlook.com>
2022-06-23 09:24:33 +0900

    init

---
+++ src/main/java/com/barroit/dev/homewardlib/HomewardLib.java
@@ -0,0 +1,12 @@
+package com.barroit.dev.homewardlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomewardLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
