d37b43cf06e4f186b68a27101035dff7b33df14d
Ba1oretto <sunjiamu@outlook.com>
2022-06-28 03:48:00 +0900

    1

---
+++ src/main/java/dev/barroit/homewardlib/HomewardLib.java
@@ -0,0 +1,12 @@
+package dev.barroit.homewardlib;
+
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomewardLib extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+    }
+
+}
